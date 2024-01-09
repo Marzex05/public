5c7e39932c9bb772c1111e04f1c5e2c8e366e015
Pretzz <88212436+Marzex05@users.noreply.github.com>
2024-01-09 17:52:34 -0500

    Add files via upload

---
+++ redirects.js
@@ -0,0 +1,13 @@
+function RedirectListings(){
+    window.location="listings.html"
+}
+
+function RedirectHome(){
+    window.location="index.html"
+
+}
+
+function RedirectError404(){
+    window.location="otherPages/404.html"
+
+}
